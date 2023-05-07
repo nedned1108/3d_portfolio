6b8729a06ea60d7b71c6598936a813d5fd225861
nednguyen <rpmshockwave@gmail.com>
2023-05-07 16:19:02 -0400

    tech section

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -7,12 +7,31 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl])
 
   return (
-    <Float>Ball</Float>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2} >
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]}/>
+      <mesh castShadow receiveShadow scale={2.75} >
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial 
+          color={'#fff8eb'}
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          map={decal}
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          flatShading
+        />
+      </mesh>
+    </Float>
   )
 }
 
 const BallCanvas = ({ icon }) => {
   return (
+  
     <Canvas
       frameLoop='demand'
       gl={{ preserveDrawingBuffer: true }}
