c2ddd756261a345e5085a5bed522d27c4b1ef627
nednguyen <rpmshockwave@gmail.com>
2023-05-07 12:25:19 -0400

    add point

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -13,7 +13,7 @@ const ExperienceCard = ({experience}) => (
     date={experience.date}
     iconStyle={{ background: experience.iconBg }}
     icon={
-      <div>
+      <div className='flex justify-center items-center w-full h-full' >
         <img 
           src={experience.icon} 
           alt={experience.company_name} 
@@ -23,11 +23,22 @@ const ExperienceCard = ({experience}) => (
     }
   >
     <div>
-      <h3
-        className='text-white text-[24px] font-bold'
-      >
+      <h3 className='text-white text-[24px] font-bold' >
         {experience.title}
       </h3>
+      <p className='test-secondary tet-[16px] font-semibold' style={{margin: 0}} >
+        {experience.company_name}
+      </p>
+      <ul className='mt-5 list-disc ml-5 space-y-2'>
+        {experience.points.map((point, index) => (
+          <li 
+            key={`experience-point-${index}`} 
+            className='text-white-100 text-[14px] pl-1 tracking-wider' 
+          >
+            {point}
+          </li>
+        ))}
+      </ul>
     </div>
   </VerticalTimelineElement>
 )
