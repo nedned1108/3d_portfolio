153f8db24982be4b0cb978280987bb78dba3d51b
nedned1108@github.com <rpmshockwave@gmail.com>
2023-09-06 15:43:04 -0400

    updated navbar

--- src/constants/index.js
+++ src/constants/index.js
@@ -43,8 +43,12 @@ export const navLinks = [
     title: "About",
   },
   {
+    id: "experience",
+    title: "Experience",
+  },
+  {
     id: "work",
-    title: "Work",
+    title: "Projects",
   },
   {
     id: "feedback",
