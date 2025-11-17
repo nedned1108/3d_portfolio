3d5e294d82557d66a70463c6bcdccd4c5c4b45fc
Truong (Ned) Nguyen <112263162+nedned1108@users.noreply.github.com>
2025-11-16 23:29:18 -0500

    Comment out image element in Works component
    Commented out the image element for future reference.

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -15,7 +15,7 @@ const ProjectCard = ({ index, name, descriptions, image, tags, source_code_link,
       className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
     >
       <div className='relative w-full h-[230px]' >
-        <img src={image} alt={name} className='w-full h-full object-cover rounded-2xl' />
+        {/* <img src={image} alt={name} className='w-full h-full object-cover rounded-2xl' /> */}
         <div className='absolute inset-0 flex justify-end m-3 card-img_hover' >
           <div 
             onClick={() => window.open(source_code_link, '_blank')}
