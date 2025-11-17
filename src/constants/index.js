12ee8d017e9aa27e5a692d9ef03d0438435b7058
Truong (Ned) Nguyen <112263162+nedned1108@users.noreply.github.com>
2025-11-16 22:35:38 -0500

    update constant
    update constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -62,10 +62,6 @@ export const navLinks = [
 
 const services = [
   {
-    title: "Web Developer",
-    icon: web,
-  },
-  {
     title: "Full-stack Software Engineer",
     icon: backend,
   },
@@ -77,16 +73,32 @@ const services = [
     title: "Backend Developer",
     icon: creator,
   },
+  {
+    title: "Web Developer",
+    icon: web,
+  },
 ];
 
 const technologies = [
+  {
+    name: "TypeScript",
+    icon: typescript
+  },
   {
     name: "Java",
     icon: java
   },
   {
+    name: "C#",
+    icon: csharp
+  },
+  {
     name: "Spring Boot",
     icon: spring,
+  },
+  {
+    name: ".NET",
+    icon: net,
   },
   {
     name: "SQL",
@@ -129,14 +141,6 @@ const technologies = [
     icon: express,
   },
   {
-    name: "Flask",
-    icon: flask,
-  },
-  {
-    name: "Three JS",
-    icon: threejs,
-  },
-  {
     name: "PostgreSQL",
     icon: postgresql,
   }
@@ -144,13 +148,13 @@ const technologies = [
 
 const experiences = [
   {
-    title: "Bachelor's Degree of Science in Health Science",
+    title: "Bachelor's Degree of Science",
     company_name: "Clayton State University",
     icon: csu,
     iconBg: "#E6DEDD", //#383E56
     date: "Jan 2019 - July 2022",
     points: [
-      "Bachelor degree of Science in Health Sciences with 3.3 GPA",
+      "Bachelor degree of Science",
     ],
   },
   {
@@ -163,17 +167,6 @@ const experiences = [
       "Coordinate 4+ projects to improve processes and workflow operations while ensuring compliance with company, general contractors, and subcontractors resulting in a net profit increase of over $100,000.",
       "Planned, established and communicated project deliverables to meet deadlines and maintain project schedules.",
       "Worked with project teams and contractors to ensure effective collaboration and timely completion of tasks.",
-    ],
-  },
-  {
-    title: "Internship",
-    company_name: "Boat People SOS",
-    icon: bpsos,
-    iconBg: "#E6DEDD", //#383E56
-    date: "December 2021 - April 2022",
-    points: [
-      "Utilized Practice Fusion, a cloud-based health record platform to create and update health histories for 100+ new patients.",
-      "Coordinated between patients and doctors to ensure effective communication and streamlined care.",
     ],
   },
   {
@@ -203,12 +196,29 @@ const experiences = [
     company_name: "Cognixia USA",
     icon: cognixia,
     iconBg: "#E6DEDD",
-    date: "June 2023 - current",
+    date: "June 2023 - December 2023",
     points: [
+      "Developed a full suite Spring Boot REST API, implementing Spring Security, Hibernate, JPA, and Mockito Test Suite.",
       "Database Design - designed databases under principles of Normalization and ACID and SOLID patterns.",
-      "Develop a full suite Spring Boot REST API, implementing Spring Security, Hibernate, JPA, and Mockito Test Suite",
-      "Collaborated with multiple cross-functional teams of 2 - 3 developers contributing to effectively deliver multiple full-stack web applications",
-      "Participated in 3+ projects following Agile/Scrum methodology to plan, implement, develop and test"
+      "Managed multiple teams of 2 - 3 developers contributing to effectively deliver a range of full-stack web applications.",
+      "Followed Agile/Scrum methodology to plan, implement, develop and test 3+ projects."
+    ],
+  },
+  {
+    title: "Full-stack Software Engineer",
+    company_name: "Fleet Hoster",
+    icon: fleethoster,
+    iconBg: "#383E56",
+    date: "December 2023 - current",
+    points: [
+      "Built multiple GEOTAB-integrated applications serving thousands of users for telemetry and fleet data analysis using Angular, TypeScript, and .Net Core in combination with SQL Server and Azure Cosmos Database.",
+      "Worked closely with cross-functional teams to develop an Internal Order Management application in .NET Core and Angular, streamlining order updates for Operations and Support teams.",
+      "Partnered with a team of four to design and deliver a full-stack internal application for billing, subscriptions, and camera management, driving a 70% productivity improvement across the company.",
+      "Developed a fully integrated Shopify storefront with Business Central connector to automate product import/export workflows and inventory management, resulting in a 20% increase in sales.",
+      "Created 6+ n8n-based AI agents to oversee and update 4,000+ dash cameras, enabling automated workflows and real-time alerts.",
+      "Led a team of 3 to deliver 20+ marketing websites using React, Node.js and Zoho CRM with a centralized configuration system for streamlined management and updates in under 4 weeks, contributing to an 25% increase in overall brand recognition.",
+      "Engineered and deployed full-stack applications to support over 7,000 fleet vehicles, improving safety metrics and reducing incident response time by 20%.",
+      "Collaborated in a 6-person agile team, contributing to code reviews, CI/CD improvements, and cross-platform integrations."
     ],
   },
 ];
@@ -234,109 +244,81 @@ const testimonials = [
 
 const projects = [
   {
-    name: "Shootstarter",
+    name: "FuelBI",
     descriptions: [
-      "Project funding website inspired by Kickstarter using Python and Flask server",
-      "A web application that allows users to create and fund crowdfunding projects.",
-      "Managed the frontend state of application using React and Redux.",
-      "Customized multiple frontend functions to handle data and ensure a seamless user experience."
+      "Re-architected both front-end and backend systems to support millions of real-time fuel transaction tracking per week and fraud detection across thousands of fleet vehicles.",
+      "Visualized fuel KPIs, transaction maps and reports for 40+ enterprise customers, improving transparency and audit efficiency."
     ],
     tags: [
       {
-        name: "python",
+        name: "angular",
         color: "blue-text-gradient",
       },
       {
-        name: "flask",
+        name: "TypeScript",
         color: "green-text-gradient",
       },
       {
-        name: "react",
+        name: "C#",
         color: "pink-text-gradient",
       },
+      {
+        name: ".NET",
+        color: "blue-text-gradient",
+      },
     ],
-    image: shootstarter,
-    source_code_link: "https://github.com/nedned1108/capstone-project-shootstarter",
-    liveLink: "https://shootstarter.onrender.com",
+    image: fuelbi,
+    source_code_link: "",
+    liveLink: "",
   },
   {
-    name: "Social Account Manager",
+    name: "Fleetflix and CoachingBI",
     descriptions: [
-      "Back-end project that allows users to manage their social media accounts.",
-      "Led a team of 3 building the project using Java and Spring Boot in 4 days.",
-      "Managed Authentication and Authorization using Spring Security to ensure the security of the application.",
-      "Tested the application using Mockito and JUnit to improve the reliability of the application.",
+        "Designed a feature enabling users to browse camera footage via an interactive timeline and seamlessly request media from any device.",
+        "Optimized performance for a smart dashcam management platform, reducing loading by 80% and able to handle 1000+ devices with ease."
     ],
     tags: [
       {
-        name: "java",
+        name: "JavaScript",
         color: "blue-text-gradient",
       },
       {
-        name: "springboot",
+        name: "Blazor",
         color: "green-text-gradient",
       },
       {
-        name: "mockito",
+        name: "AWS",
         color: "pink-text-gradient",
       },
     ],
-    image: social_media_management,
-    source_code_link: "https://github.com/nedned1108/Social-Media-Accounts-Management",
-    liveLink: "https://github.com/nedned1108/Social-Media-Accounts-Management",
+    image: fleetflix,
+    source_code_link: "",
+    liveLink: "",
   },
   {
-    name: "Gulp",
+    name: "TrackerBI & ColdChainBI",
     descriptions: [
-      "Crowd-sourced reviews about businesses using Python and Flask server",
-      "Led a group of four software engineers through the planning and building phase of Gulp",
-      "Integrated Google Maps API to provide business locations, improving the website’s functionality",
-      "Designed search features and categories for a user-friendly interface",
+      "Delivered real-time GPS asset tracking solutions for trailers, containers, and equipment, improving asset visibility and reducing loss risk.",
+      "Engineered interactive map dashboard to visualize real-time GPS data from trailers, containers and equipment, resolving 3 critical bugs improving response times to location incidents by 25%.",
     ], 
     tags: [
       {
-        name: "javascript",
+        name: "typescript",
         color: "blue-text-gradient",
       },
       {
-        name: "react",
+        name: "C#",
         color: "green-text-gradient",
       },
       {
-        name: "googleapi",
+        name: ".NET",
         color: "pink-text-gradient",
       },
     ],
-    image: gulp,
-    source_code_link: "https://github.com/glwo/pong-grp10-proj",
-    liveLink: "https://gulp.onrender.com/",
-  },
-  {
-    name: "Mybnb",
-    descriptions: [
-      "Platform for users to list rental properties, book rentals, and leave reviews",
-      "Set up Express RESTful API for streamlined data management and operation",
-      "Connected to PostgreSQL Render backend to store and update database",
-      "Ensured the functionality and reliability of endpoints, responses, and error handling by conducting API testing with Postman."
-    ],
-    tags: [
-      {
-        name: "react",
-        color: "blue-text-gradient",
-      },
-      {
-        name: "express",
-        color: "green-text-gradient",
-      },
-      {
-        name: "postgresql",
-        color: "pink-text-gradient",
-      },
-    ],
-    image: mybnb,
-    source_code_link: "https://github.com/nedned1108/API-project",
-    liveLink: "https://airbnb-api-project-znib.onrender.com/",
-  },
+    image: trackerbi,
+    source_code_link: "",
+    liveLink: "",
+  }
 ];
 
 export { services, technologies, experiences, testimonials, projects };
